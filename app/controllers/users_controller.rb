0bfd258301194079f0505fbbafe14db3147750b0
dsmodic <dsmodic@chicagobooth.edu>
2021-04-29 01:28:41 +0000

    Removed ability to see non-followed private users likes from their show page. Removed ability to see the feed and discover pages for other users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
+  before_action :check_if_current_user, only: %i[ feed discover ]
 
+  
   private
 
     def set_user
@@ -10,4 +12,13 @@ class UsersController < ApplicationController
         @user = current_user
       end
     end
+
+    def check_if_current_user
+      if params[:username]
+        if params.fetch(:username) != current_user.username
+          redirect_back fallback_location: "/#{current_user.username}/feed", alert: "You can not view the feed or discover pages of other users."
+        end
+      end
+    end
+
 end
